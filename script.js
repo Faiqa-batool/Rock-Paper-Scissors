0c9713e757b60d337eebf8b7907c4828f2308733
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-09 01:09:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+let userScore = 0;
+let compScore = 0;
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx]; //compChoice
+};
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+
+const drawGame = () => {
+  console.log("Game was draw");
+  msg.innerText = "The Game was draw! Play again";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) =>{
+    if (userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lose! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) => {
+  // generate computer choice
+  const compChoice = genCompChoice();
+  if (userChoice === compChoice) {
+    // Draw game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      // scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      // rock, scissor
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+        // rock, paper 
+        userWin = compChoice === "rock" ? false: true;   
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+choices.forEach((choice) => {
+  // console.log(choice)
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    // console.log("choice was clicked", userChoice);
+    playGame(userChoice);
+  });
+});
 
